f0f60166bf7be9e788ebc4cdbd56a986a9c87b17
shawlynot <88781957+shawlynot@users.noreply.github.com>
2024-05-06 15:22:50 +0100

    plots and data

--- shawlynot_quant/api.py
+++ shawlynot_quant/api.py
@@ -4,22 +4,27 @@ from polygon.rest.models import Agg
 import os
 import pandas as pd
 
-
 class PolygonClient:
     client: RESTClient
-    
+
     def __init__(self) -> None:
         self.client = RESTClient(os.environ.get("POLYGON_KEY"))
 
     def historical_for_tickers(self, tickers: list[str], start: date, end: date) -> pd.DataFrame:
         out: dict = {}
         for ticker in tickers:
-            from_api: list[Agg] = self.client.get_aggs(ticker=ticker, from_=start, to=end, multiplier=1, timespan="day")
-            out[ticker] = pd.Series([agg.open for agg in from_api], index=[agg.timestamp for agg in from_api])
+            from_api: list[Agg] = self.client.get_aggs(
+                ticker=ticker, from_=start, to=end, multiplier=1, timespan="day")
+            out[ticker] = pd.Series([agg.open for agg in from_api], index=[
+                                    agg.timestamp for agg in from_api])
         return pd.DataFrame(out)
-        
-            
-            
+
+
 if __name__ == "__main__":
+    stocks = ['AAPL', 'PG', 'GOOG', 'NVO', 'JPM']
+    start_date = date(2022, 5, 1)
+    end_date = date(2024, 5, 1)
+
     client = PolygonClient()
-    print(client.historical_for_tickers(["AAPL", "GOOG"], date(2022, 5, 1), date(2024, 5, 1)))
+    data = client.historical_for_tickers(stocks, start_date, end_date)
+    data.to_parquet(".cache/stocks")
--- shawlynot_quant/markowitz.py
+++ shawlynot_quant/markowitz.py
@@ -1,9 +1,13 @@
-
-
 from datetime import date
+from shawlynot_quant.api import PolygonClient
+import pandas as pd
+import numpy as np
 
+def calculate_return(data: pd.DataFrame) -> pd.DataFrame:
+    log_return = np.log(data/data.shift(1))
+    return pd.DataFrame(log_return)[1:]
 
-stocks = ['AAPL', 'WMT', 'TSL', 'GE', 'AMZN', 'DB']
-
-start_date = date(2022, 5, 1)
-end_date = date(2024, 5, 1)
+if __name__ == "__main__":
+    data = pd.read_parquet(".cache/stocks")
+    data.plot().figure.savefig(".cache/stocks.png")
+    calculate_return(data).plot().figure.savefig(".cache/returns.png")
